7968790bb2b26afc6f474dfbdc8a5fcc495b98fa
ThatCyberGuy5 <61467648+cnenno@users.noreply.github.com>
2021-11-07 10:14:08 -0600

    Update batch_hunt_script.bat
    updated syntax and grammar

--- batch_hunt_script.bat
+++ batch_hunt_script.bat
@@ -1,1 +1,3 @@
-@echo off & echo net user & net localgroup & sc query & ipconfig & nbtstat -a %computername% & systeminfo & dir c:\windows\system32 >results.txt  
+### general script to output information on what your Windows system information has!
+
+@echo off & echo net user & net localgroup & sc query & ipconfig & nbtstat -a %computername% & systeminfo & dir c:\windows\system32 > results.txt  
